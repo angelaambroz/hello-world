d7dfbcb16950094efab361b8ce1299d16f9ae6b5
Angela Ambroz <angela.ambroz@gmail.com>
2014-08-10 14:07:13 +0300

    baby steps in python

---
+++ pyforecon.py
@@ -0,0 +1,32 @@
+#Task 10
+
+myList = ["apples","bananas","oranges"]
+for item in myList:
+    print item
+
+#Task 11
+
+for num in range(50,101):
+    print num
+
+#Task 12
+
+def evaluate(name):
+    if name=="Stata":
+        print "Cool!"
+    elif name=="Python":
+        print "Also cool!"
+    else:
+        print "Lame."
+
+#Task 13
+myList = [-2,-1,0,1,2]
+for item in myList:
+    print item
+    if item<0:
+        print "negative"
+    elif item>0:
+        print "positive"
+    else:
+        print "zero"
+    
